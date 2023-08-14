a6711e3985e3af1242456470cb3194b3354e2e65
Jigme Tenzin Chogyel <jigmetenzinchogyel@gmail.com>
2023-08-14 21:49:51 +0600

    a lot of things

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Features from './components/Features'
 import Hero from './components/Hero'
 import Message from './components/Message'
 import Navbar from './components/Navbar'
+import Questions from './components/Questions'
 import Quote from './components/Quote'
 import Story from './components/Story'
 import Teachers from './components/Teachers'
+import Footer from './components/footer'
 import Stats from './components/stats'
 
 function App() {
@@ -22,6 +24,8 @@ function App() {
       <Directions />
       <Stats />
       <Teachers />
+      <Questions />
+      <Footer />
     </div>
   )
 }
